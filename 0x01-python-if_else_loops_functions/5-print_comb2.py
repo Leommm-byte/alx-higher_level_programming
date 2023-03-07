6215ad88e6128901c66fdd51cec52087285027b9
Leommm-byte <leom20031@gmail.com>
2023-03-07 07:41:21 -0800

    Updated 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(0, 100):
     if i == 99:
         print(i)
     else:
-        print(f"{int(i / 10)}{i % 10}", end=", ")
+        print("{x}{y}".format(x=int(i / 10), y=i % 10), end=", ")
